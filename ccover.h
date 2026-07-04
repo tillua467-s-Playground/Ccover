d91f379edc91b3d4c29e346c625ab173529d51b6
tillua467 <tillua467@gmail.com>
2026-07-04 13:07:51 +0600

    ccover: Add APIC format checker
    Signed-off-by: tillua467 <tillua467@gmail.com>

--- ccover.h
+++ ccover.h
@@ -9,6 +9,12 @@
 #ifndef CCOVER_H
 #define CCOVER_H
 
+enum FORMAT
+{
+    PNG = 1,
+    JPG
+};
+
 #include <stdbool.h>
 
 #ifdef __cplusplus
@@ -17,6 +23,7 @@ extern "C"
 #endif
 
     int HasApic(char *fileDir);
+    enum FORMAT CheckFormat(char *fileDir);
 
 #ifdef __cplusplus
 }
@@ -31,6 +38,7 @@ extern "C"
 #include <string.h>
 
 static unsigned char ccover__APIC_SPEC[] = {0x41, 0x50, 0x49, 0x43};
+static unsigned char ccover__PNG_SPEC[] = {0x69, 0x6D, 0x61, 0x67, 0x65, 0x2F, 0x70, 0x6E, 0x67};
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -64,6 +72,41 @@ int HasApic(char *fileDir)
     free(buffbin);
     fclose(f);
     return hasapic;
+}
+
+enum FORMAT CheckFormat(char *fileDir)
+{
+    if (HasApic(fileDir))
+    {
+        bool isPng, isJpg;
+
+        FILE *f = fopen(fileDir, "rb");
+
+        fseek(f, 0L, SEEK_END);
+        long fsz = ftell(f);
+        rewind(f);
+
+        char *buffBin = (char *)malloc(fsz);
+        fread(buffBin, 1, fsz, f);
+        int PngSize = sizeof(ccover__PNG_SPEC);
+        for (long i = 0; i < fsz - PngSize; i++)
+        {
+            if (memcmp(buffBin + i, ccover__PNG_SPEC, PngSize) == 0)
+            {
+                isPng = true;
+                break;
+            }
+        }
+
+        if (isPng)
+            return PNG;
+        else
+            return JPG;
+    }
+    else
+    {
+        printf("Error: File doesn't have Attached Picture\n");
+    }
 }
 
 #endif /* CCOVER_IMAGE_IMPLEMENTATION */
